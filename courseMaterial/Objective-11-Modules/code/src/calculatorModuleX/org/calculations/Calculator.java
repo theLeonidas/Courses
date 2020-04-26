0090899e99f646ce1de77c939432c30cd9002291
somp-it <somp.helpdesk@gmail.com>
2020-04-26 10:42:52 +0530

    ppt review and modification

---
+++ courseMaterial/Objective-11-Modules/code/src/calculatorModuleX/org/calculations/Calculator.java
@@ -0,0 +1,8 @@
+package org.calculations;
+
+public class Calculator{
+	public float add(float x, float y){return x+y;}
+	public float subtract(float x, float y){return x-y;}
+	public float multiply(float x, float y){return x*y;}
+	public float divide(float x, float y){return x/y;}		
+}
