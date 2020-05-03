b53e2409a036bd7812c6743452ce4310a7b4cad2
Salil Misra <salil.learner@gmail.com>
2020-05-03 20:34:48 +0530

    Committing remaining changes.

--- OCP/Interfaces/src/main/java/org/Interface/inheritance/ParentInterfaceB.java
+++ OCP/Interfaces/src/main/java/org/Interface/inheritance/ParentInterfaceB.java
@@ -4,6 +4,8 @@ public interface ParentInterfaceB {
     Float intConstant = 200f;
 
     default void defaultMethod(){
+        Integer intValue = new Integer(11);
+        Object objValue = intValue;
         System.out.println("Default method form Interface B.");
     }
 }
