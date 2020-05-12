a74053f51e7e7a907d3fdf8fed7b9e4a116001de
Salil Misra <salil.learner@gmail.com>
2020-05-12 11:57:34 +0530

    Committing changes add lecture notes.

--- OCP/src/main/java/WelcomeToJava.java
+++ OCP/src/main/java/WelcomeToJava.java
@@ -1,6 +1,14 @@
 public class WelcomeToJava {
+
+    String message;
+
+    public void printMessage(){
+        System.out.println(message);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Java !!!!!");
-        System.out.println("I am attending course on Java 11.");
+        WelcomeToJava myFirstObject = new WelcomeToJava();
+        myFirstObject.message = "Welcome to Java !!";
+        myFirstObject.printMessage();
     }
 }
--- OCP/src/main/java/org/java/commandline/CommandLineArguments.java
+++ OCP/src/main/java/org/java/commandline/CommandLineArguments.java
@@ -7,8 +7,9 @@ package org.java.commandline;
 //no arguments.
 public class CommandLineArguments{
     public static void main(String args[]){
-        for(int i=0;i<args.length;i++){
-           System.out.print(args[i] + ",");
-        }
+        
+        //for(int i=0;i<args.length;i++){
+          // System.out.print(args[i] + ",");
+        //}
     }
 }
